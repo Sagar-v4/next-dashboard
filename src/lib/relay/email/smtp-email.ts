c0c4a5446e3197839601c628d589aaf2ca1c1685
Sagar Variya <77156364+Sagar-v4@users.noreply.github.com>
2024-01-13 01:34:07 +0530

    removed: iritating console log

--- src/lib/relay/email/smtp-email.ts
+++ src/lib/relay/email/smtp-email.ts
@@ -27,9 +27,7 @@ export const sendSMTPMail = async ({ to, name, subject, body }: Mail) => {
         pass: SMTP_PASSWORD,
       },
     });
-    console.log("🚀 ~ file: mail.ts:26 ~ sendMail ~ transport:", transport);
     const testResult = await transport.verify();
-    console.log("🚀 ~ file: mail.ts:28 ~ sendMail ~ testResult:", testResult);
 
     const sendResult = await transport.sendMail({
       from: SMTP_EMAIL,
@@ -37,7 +35,6 @@ export const sendSMTPMail = async ({ to, name, subject, body }: Mail) => {
       subject,
       html: body,
     });
-    console.log("🚀 ~ file: mail.ts:37 ~ sendMail ~ sendResult:", sendResult);
     return sendResult.response.includes("2.0.0 OK") as boolean;
   } catch (error) {
     console.log("🚀 ~ file: mail.ts:39 ~ sendMail ~ error:", error);
